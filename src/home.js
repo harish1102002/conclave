352f169d8ca274517ef52870592407407fc748b1
HARISH R P <harishpranesh983@gmail.com>
2023-03-05 17:47:23 +0530

    hii

--- src/home.js
+++ src/home.js
@@ -73,7 +73,7 @@ function Home(props)
         if(r.current==e.send)
         seti(y)
         else
-        setn("block");
+        setn("flex");
         
     },[post])
     useEffect(()=>scr(),[inchat])
